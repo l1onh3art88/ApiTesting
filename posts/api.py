d2b6e9f96113da2eb2a5b7e6db51a39a9aa99a4b
Spencer Cheng <spenccheng@gmail.com>
2016-07-20 19:33:53 +0000

    create read update delete tests and api complete

--- posts/api.py
+++ posts/api.py
@@ -8,6 +8,15 @@ from . import decorators
 from posts import app
 from .database import session
 
+post_schema = {
+    "properties": {
+        "title": {"type": "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
+
 @app.route("/api/posts", methods = ["GET"])
 @decorators.accept("application/json")
 def posts_get():
@@ -63,3 +72,64 @@ def delete_post(id):
     posts = session.query(models.Post).first()
     data = json.dumps(posts.as_dictionary())
     return Response(data, 200, mimetype ="application/json")
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+    
+    #Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    
+    try: 
+        validate(data, post_schema)
+    except ValidationError as error:
+        data={"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+    
+    
+    #Add the post to the database
+    
+    post = models.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+    
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers, mimetype="application/json")
+    
+@app.route("/api/posts/<int:id>", methods=["PUT"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_put(id):
+    """ Edits a post """
+    post = session.query(models.Post).get(id)
+    data = request.json
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data,404, mimetype = "application/json")
+        
+        
+    try: 
+        validate(data, post_schema)
+    except ValidationError as error:
+        data={"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+        
+    #Edits the post
+    post.title="new example A"
+    post.body="new example body"
+    session.add(post)
+    session.commit()
+    
+    #Return a 200 updated, containing the post as JSON and with the 
+    # Location header set to the locatin of the post
+    
+    data = json.dumps(post.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
